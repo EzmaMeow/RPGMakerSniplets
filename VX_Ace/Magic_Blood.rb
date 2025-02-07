e0532edb00340be365f2c310fce5d608d9ca897b
Ezma Green <EzmaMeow@hotmail.com>
2025-02-07 16:21:43 -0500

    Update Magic_Blood.rb
    Change the logic to flag certain skills as bloodmagic instead of being a class feature. Also remove the mp usage and instead convert the mp cost as hp cost. I may or may not keep the MP_TO_HP_RATIO. Considering hp usually goes up to 4 digits while mp may only go up to 3(or was it 2), I may just keep it.

--- VX_Ace/Magic_Blood.rb
+++ VX_Ace/Magic_Blood.rb
@@ -1,45 +1,44 @@
 class Magic_Blood
   
   #--------------------------------------------------------------------------
-  # * Constants (Features)
-  #--------------------------------------------------------------------------
-  CLASS_ID  = 11                    #The default class to make use of blood magic
+  # * Constants
+  #-------------------------------------------------------------------------- 
   MP_TO_HP_RATIO  = 10              #Blood mages can cast spells without mp
-  MP_EFFICIENCY = 0.2               #Blood magic cost hp, mp just reduces it
+  HP_TO_POWER_RATIO = 250           #Higher the HP cost, the better the dmg
+  
+  BLOOD_MAGIC_SKILLS_START = 127   #to reduce stack reading, can provide a range
+  BLOOD_MAGIC_SKILLS_END = 129     #but after the range, will need to add to stack
+  BLOOD_MAGIC_SKILLS = [127]       #uses mp_to_hp ratio for hp cost
   
   #--------------------------------------------------------------------------
-  # * States if the class is a bloodmage
+  # * States if the skill is blood magic
   #--------------------------------------------------------------------------
-  def self.is_bloodmage?(class_id)
-    class_id == CLASS_ID
-  end
-  
-  def self.get_hp_cost(skill,caster)
-    #need to see how much the mp reduce the hp cost
-    remainer = caster.skill_mp_cost(skill) - caster.mp 
-    print " | "
-    print remainer
-    if remainer < 0
-      #mp was not used up, hp cost will be less
-      return (caster.skill_mp_cost(skill)*MP_TO_HP_RATIO*MP_EFFICIENCY).floor 
+  def self.is_blood_magic?(skill)
+    if skill.id >= BLOOD_MAGIC_SKILLS_START && skill.id <= BLOOD_MAGIC_SKILLS_END
+      true
+    else
+      BLOOD_MAGIC_SKILLS.include?(skill.id)
     end
-    return (remainer*MP_TO_HP_RATIO).floor
+  end
+  #--------------------------------------------------------------------------
+  # * get the hp cost of casting a blood spell
+  #--------------------------------------------------------------------------
+  def self.get_hp_cost(skill,caster)
+    return caster.skill_mp_cost(skill)*MP_TO_HP_RATIO
   end
   #--------------------------------------------------------------------------
   # * Determine if Cost of Using Skill Can Be Paid
   #--------------------------------------------------------------------------
   def self.skill_cost_payable?(skill,caster)
     hp_cost = get_hp_cost(skill,caster)
-    caster.tp >= caster.skill_tp_cost(skill) && 
-    (caster.mp >= caster.skill_mp_cost(skill) && caster.hp > hp_cost||
-    caster.hp > hp_cost)
+    #NOTE: not checking mp since blood magic use hp to cast
+    caster.tp >= caster.skill_tp_cost(skill) && caster.hp > hp_cost
   end
   #--------------------------------------------------------------------------
   # * Pay Cost of Using Skill
   #--------------------------------------------------------------------------
   def self.pay_skill_cost(skill,caster)
     caster.hp -= get_hp_cost(skill,caster)
-    caster.mp -= caster.skill_mp_cost(skill)
     caster.tp -= caster.skill_tp_cost(skill)
   end
   
@@ -48,26 +47,19 @@ class Magic_Blood
   #--------------------------------------------------------------------------
   def self.draw_skill_cost(rect, skill, actor, source)
     hp_cost = get_hp_cost(skill,actor)
-    remainer = actor.skill_mp_cost(skill) - actor.mp
-    mp_cost = [actor.skill_mp_cost(skill), remainer].max
     text = ""
     if hp_cost > 0
       source.change_color(source.hp_gauge_color2, source.enable?(skill))
       source.draw_text(rect, hp_cost, 2 )
       rect.x -= 8 * hp_cost.to_s.length + 8
-      #text = hp_cost.to_s + "hp"
+    elsif actor.skill_mp_cost(skill) > 0
+      source.change_color(source.mp_cost_color, source.enable?(skill))
+      source.draw_text(rect, actor.skill_mp_cost(skill), 2 )
     end
     if actor.skill_tp_cost(skill) > 0
       source.change_color(source.tp_cost_color, source.enable?(skill))
       source.draw_text(rect, actor.skill_tp_cost(skill), 2 )
       rect.x -= 8 *  actor.skill_tp_cost(skill).to_s.length + 8
-      #text = actor.skill_tp_cost(skill).to_s + "sp " + text
     end
-    if actor.skill_mp_cost(skill) > 0
-      source.change_color(source.mp_cost_color, source.enable?(skill))
-      source.draw_text(rect, actor.skill_mp_cost(skill), 2 )
-      #text = mp_cost.to_s + "mp " + text
-    end
-    #source.draw_text(rect, text, 2)
   end
 end
