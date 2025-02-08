1494b0b12e0af3662ec743b959b359508a9cecb1
Ezma Green <EzmaMeow@hotmail.com>
2025-02-08 03:55:18 -0500

    Update Calculations_Formulas.rb
    Added power_attack and change basic_attack to attack so it did not take up as much space.

--- VX_Ace/Calculations_Formulas.rb
+++ VX_Ace/Calculations_Formulas.rb
@@ -1,11 +1,19 @@
 class Formula
-  
+  #returns a value base on the ratio of the stat and max stat offset 
+  #by the provided base.
   def self.get_modifier(stat, base = 1.0, max=255.0)
     return (stat.to_f/max.to_f) + base
   end
-
-  def self.basic_attack(damage, attack, defence)
+  
+  #The default attack logic. It damage is modify around 0-2 times
+  #base on the attack stat
+  def self.attack(damage=1, attack=1, defence=0)
     return damage * get_modifier(attack-defence,1.0)
+  end
+  
+  #An attack that damage scales off of atk stat
+  def self.power_attack(power=1, attack=1, defence=0)
+    return power * [(attack-defence),1].max
   end
 
 end
