42db691e00a90c52c49629a96bd6372ff91ee532
Ezma Green <EzmaMeow@hotmail.com>
2025-02-11 21:22:51 -0500

    Update Sp_WeaponClasses.rb
    reset the Constants to use default classes and weapon id than my project as well as remove a redundant line

--- VX_Ace/Sp_WeaponClasses.rb
+++ VX_Ace/Sp_WeaponClasses.rb
@@ -2,17 +2,18 @@ class Sp_WeaponClasses
   #--------------------------------------------------------------------------
   # * Constants
   #--------------------------------------------------------------------------
-  WEAPON_CLASSES = [nil,1,2,3,4,5,6,7,8,9,10] #index:weapon_type_id, value:class_id
-  KEEP_EXP = false                  #enable or disable shared exp between classes
+  ##index:weapon_type_id, value:class_id
+  WEAPON_CLASSES = [nil,1,2,3,4,5,6,7,9,10] 
+  KEEP_EXP = false  #enable or disable shared exp between classes
   #--------------------------------------------------------------------------
   # * Switch Class On Weapon Change
   #--------------------------------------------------------------------------
   def self.on_weapon_change(weapon,actor)
-    class_id = WEAPON_CLASSES[weapon.wtype_id]
-    if weapon.wtype_id >= 0 && weapon.wtype_id < WEAPON_CLASSES.length
+    if weapon.wtype_id > 0 && weapon.wtype_id < WEAPON_CLASSES.length
       class_id = WEAPON_CLASSES[weapon.wtype_id]
       if class_id && class_id != actor.class_id
         actor.change_class(class_id, KEEP_EXP)
+        actor.init_skills
         #NOTE: if KEEP_EXP = false, classes seem to have their own exp stored
       end
     end
