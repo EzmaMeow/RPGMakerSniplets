cf9bbd11fe8d67439e37da917374e5d9b135101b
Ezma Green <EzmaMeow@hotmail.com>
2025-02-19 01:14:12 -0500

    Update Event_Triggers.rb
    Added selfswitch, and init listening. Init is designed to be called once the event is created and thus events would need to add `*init` to their name to make use of it. selfswitch is an extra channel to listen too for self switch changes, but it will be called when any self switch change happens. Note: most of this auto functionality happens in the _Inj file and can be disable or modified there.
    
    Also added the ability to make triggers for manual calling. Currently limited to around 7, but MAX_TRIGGERS can be increased. Since it uses an array to keep track of events, limiting it can help accidently adding hundreds of nil elements (like naming an event with `*trig85345`. Also can register events by adding `*trig#` (where # is the trigger id) in the event name.
    Note: this system was built to work not on autorun or parallel events since they do not need remote calls.

--- VX_Ace/Event_Triggers.rb
+++ VX_Ace/Event_Triggers.rb
@@ -7,20 +7,28 @@ module Event_Triggers
   # * Constants
   #--------------------------------------------------------------------------
   #This will ignore varibles and switches of the listed indexes from staring events
-  BLACKLIST_VAR_IDS = [[],[]]  
+  BLACKLIST_VAR_IDS = [[],[],[]]
+  RESERVE_TRIGGERS = 3 
+  MAX_TRIGGERS = 10 #this is to prevent accident expansion. Increase as needed
   
   #--------------------------------------------------------------------------
   # * Module Instance Variables
   #--------------------------------------------------------------------------
-  @listeners = [[],[]] 
+  @listeners = [[],[],[],[]]
   @queue_triggers = []
-
+  
+  #will only work with the declare types
   def self.connection_type_id(name)
     if [0,"variable","variables","var"].include?(id)
-      return 1
+      return 0
     elsif [1,"switch","switches","swi"].include?(id)
+      return 1
+    elsif [2,"self_switch","self_switches","sswi"].include?(id)
       return 2
+    elsif [3,"init","start","map","ini"].include?(id)
+      return 3
     end
+    
     return 0
   end
   
@@ -28,6 +36,7 @@ module Event_Triggers
     if !@listeners[id] || !event; return; end
     if @listeners[id].include?(event); return; end
     @listeners[id].push(event)
+    puts ""
     print " id "; print id; print " "; print event; print " connected "
   end
   
@@ -36,10 +45,29 @@ module Event_Triggers
     @listeners[id].delete(event)
   end
   
-  def self.add_to_trigger_queue(id)
+  def self.add_to_trigger_queue(id, run = false)
     if !@queue_triggers.include?(id)
       @queue_triggers.push(id)
     end
+    #will run triggers. this should be true only with custom triggers
+    if run
+      run_triggers
+    end
+  end
+  
+  def self.create_trigger(id = RESERVE_TRIGGERS+1)
+    if id <= RESERVE_TRIGGERS
+      puts "unable to create trigger. The id is reserver for built in triggers"
+      return false
+    end
+    if id > MAX_TRIGGERS
+      puts "unable to create new trigger id. Increase MAX_TRIGGERS if more are needed"
+      return false
+    end
+    if !@listeners[id]
+      @listeners[id] = []
+    end
+    return true
   end
   
   def self.on_change(id = 0, index = 0, value = 0, old_value=0)
@@ -50,12 +78,15 @@ module Event_Triggers
   
   def self.run_triggers
     for trigger in @queue_triggers
-      for event in @listeners[trigger]
-        if event.respond_to?(:start)
-          if event.trigger.to_i  > 1; return; end 
-          event.start
-        else
-          @listeners[trigger].delete(event)
+      if @listeners[trigger]
+        for event in @listeners[trigger]
+          if event.respond_to?(:start)
+            if event.trigger.to_i  > 1; return; end 
+            event.start
+            @queue_triggers.delete(trigger)
+          else
+            @listeners[trigger].delete(event)
+          end
         end
       end
     end
