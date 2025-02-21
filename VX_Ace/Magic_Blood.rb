f5d56c4077ab7219bf348552a9557c83805b543a
Ezma Green <EzmaMeow@hotmail.com>
2025-02-21 00:12:27 -0500

    Update Magic_Blood.rb
    Remove the optional Note_Reader in favor of keeping the getters to be overridden in the Note_Reader_Inj

--- VX_Ace/Magic_Blood.rb
+++ VX_Ace/Magic_Blood.rb
@@ -1,5 +1,3 @@
-#optional: Uses Note_Reader if added
-
 module Magic_Blood
   #--------------------------------------------------------------------------
   # * Constants
@@ -8,48 +6,12 @@ module Magic_Blood
   MP_TO_HP_RATIO  = 10              #Blood mages can cast spells without mp
   BASE_HP_BONUS_RATE = 0.1          #this is the bonus cost(and power) from max hp
   
-  @skills_extra_data = {}
-  
-  ##Getters for the specail stats. if Note_Reader is used, then @var=value;
-  ##can be added to the skill notes to override the provided functions
-  ##Due to skill notes note being acessable easly, Note Reader is used
-  ##since it expose item notes publicly
-  ##(issue is item class is RPG::Skill and I can not seem to acess its parent
-  ##properties)
-  def self.make_skill_data(skill)
-    #NOTE: this will create a hash for each vaild skill that use this
-    #it will fill it with the default values. This might not be desired
-    #but can be removed by returning just the defaults. I may try to improve it
-    @skills_extra_data[skill] = [
-    MP_TO_HP_RATIO.to_f,
-    BASE_HP_BONUS_RATE.to_f
-    ]
-  end
-  
   def self.mp_to_hp_ratio(skill)
-    if !@skills_extra_data[skill]
-      make_skill_data(skill)
-    end
-    if defined?(Note_Reader)
-      vars = Note_Reader.get_varibles(skill)
-      if vars["mp_to_hp_ratio"] != nil
-        @skills_extra_data[skill][0] = vars["mp_to_hp_ratio"].to_f
-      end
-    end
-    return @skills_extra_data[skill][0]
+    return MP_TO_HP_RATIO.to_f
   end
   
   def self.hp_bonus_rate(skill)
-    if !@skills_extra_data[skill]
-      make_skill_data(skill)
-    end
-    if defined?(Note_Reader)
-      vars = Note_Reader.get_varibles(skill)
-      if vars["hp_bonus_rate"] != nil
-        @skills_extra_data[skill][1] = vars["hp_bonus_rate"].to_f
-      end
-    end
-    return @skills_extra_data[skill][1]
+    return BASE_HP_BONUS_RATE.to_f
   end
   
   #--------------------------------------------------------------------------
