60d957d4f4b070dc9800c1a96c02c45c206897e1
Ezma Green <EzmaMeow@hotmail.com>
2025-02-23 18:18:06 -0500

    Create Game_Map_Extension.rb

---
+++ VX_Ace/Game_Map_Extension.rb
@@ -0,0 +1,30 @@
+class Game_Map
+  
+  alias old_initialize initialize
+  alias old_setup setup
+  
+  attr_accessor :min_level
+  attr_accessor :max_level
+  
+  def initialize
+    old_initialize
+    @min_level = 1
+    @max_level = 99
+  end
+  
+  ##resets the level range. Should be called everytime a new map is loaded
+  def setup_level
+    @min_level = 1
+    @max_level = 99
+  end
+  
+  def setup(map_id)
+    old_setup(map_id)
+    setup_level
+  end
+  
+  def default_level(level)
+    [[level,@min_level].max,@max_level].min
+  end
+  
+end
