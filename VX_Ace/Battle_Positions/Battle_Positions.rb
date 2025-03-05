bb74267a6289245bf336b7b6550f60a4556242b9
Ezma Green <EzmaMeow@hotmail.com>
2025-03-05 16:22:33 -0500

    Update Battle_Positions.rb
    Added ways to handle weapons. Can use note reader and override baseItem range functions to what added in the weapons notes or assign it a different skill to use as a normal attack. 
    Note: Most cases normal attack damage type is consider a melee range. Also this depends on overriding the Base item range functions from info added in the skill or weapon notes. I did not uploaded my Note_Reader_Inj with the modications(yet).

--- VX_Ace/Battle_Positions/Battle_Positions.rb
+++ VX_Ace/Battle_Positions/Battle_Positions.rb
@@ -2,6 +2,11 @@
   # Window_ActorCommand :add_movement_commands
 #as added functionality. Ext files of those should have it.
 
+class Battle_Positions
+  #index is the wtype, value is the skill id to use as normal attack
+  WTYPE_ATTACK_SKILLS =[]
+end
+
 # * INJECTIONS  * 
 
 #--------------------------------------------------------------------------
@@ -12,6 +17,61 @@ module Vocab
   def self.move_back;      "Back";   end
 end
 
+
+#--------------------------------------------------------------------------
+# * RPG::BaseItem
+#   declaring them here so weapon can use them
+#   but since weapons range is a bonus, best return 0 and have UsableItem
+#   set the defaults for skills
+#--------------------------------------------------------------------------
+class RPG::BaseItem
+  def range
+    0
+  end
+  def min_range
+    0
+  end
+  def max_range
+    0
+  end
+end
+#--------------------------------------------------------------------------
+# * RPG::UsableItem
+#   making it so that normal attack skills have melee range
+#   as default
+#--------------------------------------------------------------------------
+class RPG::UsableItem < RPG::BaseItem
+  def range
+    if @range == nil
+      @range = 4
+      if @damage.element_id == -1
+        @range = 0
+      end
+    end
+    return @range
+  end
+  
+  def min_range
+    if @min_range == nil
+      @min_range = 0
+    end
+    return @min_range
+  end
+  
+  def max_range
+    if @max_range == nil
+      @max_range = 20
+      if @damage.element_id == -1
+        @max_range = 1
+      end
+    end
+    return @max_range
+  end
+end
+  
+#--------------------------------------------------------------------------
+# * Game_Battler
+#--------------------------------------------------------------------------
 class Game_Battler < Game_BattlerBase
   alias :old_initialize_battler_battle_positions :initialize
   alias :old_extra_damage_value_battler_battle_positions :extra_damage_value
@@ -21,8 +81,44 @@ class Game_Battler < Game_BattlerBase
   def initialize
     old_initialize_battler_battle_positions
     @position = 0
+    @min_position = 0
+    @max_position = 100
   end
-
+  
+  #allow weapons bonuses to effect normal attack skills
+  def get_range_bonuses(user,item)
+    bonuses = [0,0,0]
+    if !user.respond_to?(:weapons)
+      return bonuses
+    end
+    weapon = user.weapons[0]
+    if weapon && item.damage.element_id == -1
+      if weapon.respond_to?(:range); bonuses[0] = weapon.range; end
+      if weapon.respond_to?(:min_range); bonuses[1] = weapon.min_range; end
+      if weapon.respond_to?(:max_range); bonuses[2] = weapon.max_range; end
+    end
+    return bonuses
+  end
+  
+  #check to see if the user and item can reach
+  #could be used to check targer since math should allow it
+  def reach?(user,item)
+    bonuses = get_range_bonuses(user,item)
+    range = 4 + bonuses[0]
+    min_range = 0 + bonuses[1]
+    max_range = 20 + bonuses[2]
+    if item.respond_to?(:range); range = item.range + bonuses[0]; end
+    if item.respond_to?(:min_range); min_range = item.min_range + bonuses[1]; end
+    if item.respond_to?(:max_range); max_range = item.max_range + bonuses[2]; end
+    distance = [(@position + user.position) - range,0].max
+    #note: distance need to be 0 or greater, else min_range wont work at 0
+    print [range,min_range,max_range,distance]
+    if distance < min_range || distance > max_range
+      return false
+    end
+    return true
+  end
+  
   def change_position(new_position = 0)
     @position = new_position
   end
@@ -34,9 +130,19 @@ class Game_Battler < Game_BattlerBase
   end
   
   def apply_position_effect(value,user,item)
-    range = item.range if item.respond_to?(:range) else 4
-    distance = @position + user.position #(melee: 0+0 = 0 range, mid =1, back=2) 
-    penalty = 1.0/([distance - range + 1 ,1].max.to_f) 
+    bonuses = get_range_bonuses(user,item)
+    range = 4 + bonuses[0]
+    if item.respond_to?(:range); range = item.range; end
+    distance = (@position + user.position) - range 
+    if !reach?(user,item)
+      @result.missed = true
+      return 0
+    end
+    penalty = 1.0/([distance + 1 ,1].max.to_f)
+    if value * penalty <= 0
+      @result.missed = true
+      return 0
+    end
     return (value * penalty).to_i
   end
   
@@ -48,10 +154,14 @@ class Game_Battler < Game_BattlerBase
   end
 end
 
+#--------------------------------------------------------------------------
+# * Game_Actor
+#-------------------------------------------------------------------------
 class Game_Actor < Game_Battler
   
   alias :old_initialize_actor_battle_positions :initialize
   alias :old_on_battle_end_actor_battle_positions :on_battle_end
+  alias :old_attack_skill_id_actor_battle_positions :attack_skill_id
   
   attr_accessor  :formation_position
   
@@ -65,6 +175,23 @@ class Game_Actor < Game_Battler
     return if !respond_to?(:change_position)
     change_position(@formation_position)
   end
+  
+  #can override normal attack skills here
+  def attack_skill_id
+    #NOTE: duel weilding may be odd to work with. will focus more on the 
+    #first weapon. ideally they be one handed and use default attack
+    #which should be melee. trying to duel weild for added range bonus
+    #wont really work well since it hard to figure out what the second attack is
+    wtype_id = 0
+    if weapons[0]
+      wtype_id = weapons[0].wtype_id 
+    end 
+    if Battle_Positions::WTYPE_ATTACK_SKILLS[wtype_id]
+      return Battle_Positions::WTYPE_ATTACK_SKILLS[wtype_id]
+    end
+    return old_attack_skill_id_actor_battle_positions
+  end
+  
 end
 
 #--------------------------------------------------------------------------
@@ -124,7 +251,6 @@ class Game_Unit
   
   #return true if there a gap in the front row
   def advance?
-    #min position can be a gap or front row fliers(if logic is added)
     min_position_count = 0
     active_members = members
     if respond_to?(:battle_member)
@@ -134,10 +260,6 @@ class Game_Unit
       return false if !member.respond_to?(:position)
       #ignore dead, but not movable still counts
       next if member.dead?
-      #will forward if there no one on front lines
-      #if everyone at their min, then move forward will not
-      #change anything, but this will force backrow to move ahead of
-      #flyiers
       if member.position <= @min_position
         min_position_count += 1
       end
