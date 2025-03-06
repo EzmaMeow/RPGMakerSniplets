3b85933a03aa4e4f947c601b521ec14ac2c297cb
Ezma Green <EzmaMeow@hotmail.com>
2025-03-06 18:41:12 -0500

    Update Note_Reader.rb
    Added tagging and included a priority variable for baseItem. Tags are not used at the moment, can be used for filtering or advance sorting. Priority allow ordering items displayed in lists. This is to allow not needing to reorganize the database. The current way it is applied is a bit limiting but preserves item id in the same priority without having to presort the array by id. Priority can be overridden or alias to add custom rating calculations. \@priority is used as a catch so calculations based on static values won't need to run more than once a session.

--- VX_Ace/Note_Reader.rb
+++ VX_Ace/Note_Reader.rb
@@ -1,3 +1,6 @@
+
+# * INJECTIONS  * 
+
 ##Exposing notes in the Game_Map
 class Game_Map
   def note
@@ -15,17 +18,84 @@ end
 #to acessparty member notes: #$game_party.members[0].actor.note
 #to acess class notes: #$game_party.members[0].class.note
 
-module Note_Reader  
-  #will return a hash of var names and values as strings
-  #so the caller need to cast to the correct type since this
-  #will not know if it should be a int, float, or string
+
+#--------------------------------------------------------------------------
+# * RPG::BaseItem
+#--------------------------------------------------------------------------
+class RPG::BaseItem
+  #--------------------------------------------------------------------------
+  # * Get Tags Array 
+  #   A way to tag item base objects. Can be used as filtering for advance sorting
+  #--------------------------------------------------------------------------
+  def tags
+    if @tags == nil
+      @tags = Note_Reader.get_tags(self)
+    end
+    return @tags
+  end
+  #--------------------------------------------------------------------------
+  # * Get priority
+  #   priority acts as a diffrent group so skill
+  #   id order is perserve within the same priority
+  #--------------------------------------------------------------------------
+  def priority
+    if @priority == nil
+      @priority = Note_Reader.get_variable("priority",self,15).to_i
+    end
+    return @priority
+  end
+end
+
+#--------------------------------------------------------------------------
+# * Game_Actor
+#--------------------------------------------------------------------------
+class Game_Actor < Game_Battler
+  #--------------------------------------------------------------------------
+  # * Get Skills Array 
+  #--------------------------------------------------------------------------
+  def skills
+    default = (@skills | added_skills).collect {|id| $data_skills[id] }
+    return default.sort_by { |skill| (skill.priority*1000 + skill.id) }
+  end
+end
+
+#--------------------------------------------------------------------------
+# * Game_Party 
+#--------------------------------------------------------------------------
+class Game_Party < Game_Unit
+  #--------------------------------------------------------------------------
+  # * Get Item Object Array 
+  #--------------------------------------------------------------------------
+  def items
+    default = @items.keys.sort.collect {|id| $data_items[id] }
+    return default.sort_by { |item| (item.priority*1000 + item.id) }
+  end
+  #--------------------------------------------------------------------------
+  # * Get Weapon Object Array 
+  #--------------------------------------------------------------------------
+  def weapons
+    default = @weapons.keys.sort.collect {|id| $data_weapons[id] }
+    return default.sort_by { |item| (item.priority*1000 + item.id) }
+  end
+  #--------------------------------------------------------------------------
+  # * Get Armor Object Array 
+  #--------------------------------------------------------------------------
+  def armors
+    default = @armors.keys.sort.collect {|id| $data_armors[id] }
+    return default.sort_by { |item| (item.priority*1000 + item.id) }
+  end
+end
+
+# * Base  * 
+
+module Note_Reader
+  TAG_IDENTIFIER = '@'
+
   def self.get_variables(source)
     vars = {}
     if source.respond_to?(:note)
       for scan_results in source.note.scan(/@(.*?)=(.*?);/)
         if scan_results.size >= 2
-          #note: currenly blank key and value can be added
-          #ideally this should not hurt since blank = ""
           vars[scan_results[0]] =scan_results[1]
         end
       end
@@ -34,7 +104,7 @@ module Note_Reader
   end
   
   def self.get_variable(key,source,default = 0)
-    if key == nil; return default; end #nil keys will be a pain. 
+    if key == nil; return default; end 
     if source.respond_to?(:note)
       results = source.note.match(/@#{key}=(.*?);/)
       if results != nil
@@ -43,4 +113,12 @@ module Note_Reader
     end
     return default
   end
+  
+  def self.get_tags(source)
+    if source.respond_to?(:note)
+      return source.note.scan(/#(\w+)/).flatten
+    end
+    return []
+  end
+  
 end
